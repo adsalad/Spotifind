2dd3c28ad4c5aa6db87048cfb0695af9d1d32946
Adam S <asalaymeh@gmail.com>
2022-01-30 16:40:58 -0500

    temporary commit merge

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import { Container, Form } from "react-bootstrap";
 import useAuth from "./useAuth";
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
-  return <div>{code}</div>;
+  const [search, setSearch] = useState("");
+  return (
+    <Container>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      ></Form.Control>
+    </Container>
+  );
 }
